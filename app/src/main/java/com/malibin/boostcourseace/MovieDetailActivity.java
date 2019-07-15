1bd3e851ec99132bfd6e7c26e78ab19ffb67c3b6
Malibin <nightmare73@naver.com>
2019-07-15 19:07:29 +0900

    앱 아이콘 변경, 파일명 변경

--- app/src/main/java/com/malibin/boostcourseace/MovieDetailActivity.java
+++ app/src/main/java/com/malibin/boostcourseace/MovieDetailActivity.java
@@ -4,14 +4,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.RatingBar;
 
-public class MainActivity extends AppCompatActivity {
+public class MovieDetailActivity extends AppCompatActivity {
 
     RatingBar ratingBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_movie_detail);
 
         ratingBar = findViewById(R.id.rating_movie_detail_act_star_rating);
         ratingBar.setRating(8.2f);
