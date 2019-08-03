4643bb304d0af1ad7778e03956fed29ac4e6743e
Malibin <nightmare73@naver.com>
2019-08-03 17:58:14 +0900

    PJT3. 한줄평 목록 레이아웃 완성

---
+++ app/src/main/java/com/malibin/boostcourseace/review/more/ReviewMoreActivity.java
@@ -0,0 +1,15 @@
+package com.malibin.boostcourseace.review.more;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.malibin.boostcourseace.R;
+
+public class ReviewMoreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_review_more);
+    }
+}
